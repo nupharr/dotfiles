2476dc0c73b448c6407b05926d5aaa809b915354
nupharr <ilyakubyshkin06@gmail.com>
2025-04-12 17:18:35 +0300

    SQL support in nvim and add forlder fastfetch

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "plugins" },
   },
   defaults = {
